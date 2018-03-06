92f998676fc8e629fd543fa0c77c0dfdaa69d39e
zentim <zentim3248@gmail.com>
2018-03-06 21:29:54 +0800

    add Item's sub components

---
+++ src/components/Item/ItemContent.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout>
+    <div>
+      <!-- <tinymce id="d1" v-model="data" :other_options="editerOptions"></tinymce> -->
+      <div>data:</div>
+      <div>content:</div>
+    </div>
+  </v-layout>
+</template>
+
+
+<script>
+  export default {
+    props: ['content'],
+    data () {
+      return {
+        data: this.content,
+        editerOptions: {
+          height: 300
+        }
+      }
+    },
+    created: function () {
+      console.log('item content created')
+    },
+    destoryed: function () {
+      console.log('item content destoryed')
+    },
+    updated: function () {
+      console.log('item content updated')
+    },
+    mounted: function () {
+      console.log('item content mounted')
+    },
+    watch: {
+      '$route': function () {
+        console.log('item content watch')
+      }
+    }
+  }
+</script>
