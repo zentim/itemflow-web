70f67b3df91d62ceaf4aa94774477d81dfd77d32
zentim <zentim3248@gmail.com>
2018-03-07 22:30:51 +0800

    remove test code

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -7,23 +7,6 @@
       </div>
       <v-btn @click="onLogout" class="primary">Logout</v-btn>
     </v-flex>
-
-    <v-flex>
-      <ais-index
-        app-id="latency"
-        api-key="3d9875e51fbd20c7754e65422f7ce5e1"
-        index-name="bestbuy"
-      >
-        <ais-search-box></ais-search-box>
-        <ais-results>
-          <template slot-scope="{ result }">
-            <h2>
-              <ais-highlight :result="result" attribute-name="name"></ais-highlight>
-            </h2>
-          </template>
-        </ais-results>
-      </ais-index>
-    </v-flex>
   </v-layout>
 </template>
 
