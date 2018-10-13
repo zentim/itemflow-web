03ef048d2007b029966c1180c2c325aa28d5d8d7
zentim <zentim3248@gmail.com>
2018-10-14 01:07:08 +0800

    remove debug code

--- src/components/Flow/FlowContent.vue
+++ src/components/Flow/FlowContent.vue
@@ -68,7 +68,6 @@
           let obj = this.$store.getters.itemflowStoreObj(newVal[i].id)
 
           if (obj && !obj.deletedDate) {
-            console.log('tes')
             lastestData.push({
               id: obj.id,
               type: obj.type,
