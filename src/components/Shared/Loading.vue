7022b9af0b5fccff20b5388b72ca1bfdbad224dc
zentim <zentim3248@gmail.com>
2019-08-29 04:33:14 +0800

    REMOVE ALL CODE

--- src/components/Shared/Loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-flex xs12 class="text-xs-center">
-    <v-progress-circular
-      indeterminate
-      class="primary--text"
-      :width="7"
-      :size="70"></v-progress-circular>
-  </v-flex>
-</template>
