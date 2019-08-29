763c9662884035dd193762ea929d51d8cca302b1
zentim <zentim3248@gmail.com>
2019-08-29 13:39:46 +0800

    Fix icon

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { store } from './store'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Loading from './components/Shared/Loading'
 import Alert from './components/Shared/Alert'
 import Snackbar from './components/Shared/Snackbar'
